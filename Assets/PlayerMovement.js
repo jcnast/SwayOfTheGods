4ab4fc9616a6c65f8c1fb75095b78fe7ae33e609
jcnast <j.nast@hotmail.com>
2014-10-29 21:39:28 -0400

    Actual game files

---
+++ Assets/PlayerMovement.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+function Update () {
+	//Works except for animation continuity
+	if (Input.GetKey(KeyCode.A)) {
+		Debug.Log('a');
+		rigidbody.velocity.x = 100;
+		//animation.Play('walk');
+	}else{// if (Input.GetKey(KeyCode.D)) {
+		Debug.Log('d');
+		rigidbody.velocity.x = -100;
+		//animation.Play('walk');
+	}//else{
+	//	rigidbody.velocity.x = 0;
+	//}
+	
+	//if (Input.GetKey(KeyCode.W)) {
+	//	Debug.Log('w');
+	//	rigidbody.velocity.z = -10;
+	//	//animation.Play('walk');
+	//}else if (Input.GetKey(KeyCode.S)) {
+	//	Debug.Log('s');
+	//	rigidbody.velocity.z = 10;
+	//	//animation.Play('walk');
+	//}else{
+	//	rigidbody.velocity.z = 0;
+	//}
+
+}
